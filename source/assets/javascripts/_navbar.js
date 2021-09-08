403f29b6e72beb5c8157d34dbfeb7672eadc73d6
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-09-08 16:58:29 +0900

    added partials folder and files

--- source/assets/javascripts/_navbar.js
+++ source/assets/javascripts/_navbar.js
@@ -8,3 +8,10 @@ document.querySelector('.toggle').onclick = (event) => {
   event.currentTarget.classList.toggle('active');
   navigation.classList.toggle('active');
 };
+
+const closeNav = document.querySelector('ul#close_nav');
+const closeX = document.querySelector('.toggle');
+closeNav.addEventListener('click', (event) => {
+  event.currentTarget.parentElement.classList.remove('active');
+  closeX.classList.remove('active');
+});
